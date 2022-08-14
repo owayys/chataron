a0efead7389db303c0ad5286ea817fb11864c1c7
Owais Ahsan <owais.ahsan@hotmail.com>
2022-08-14 10:45:08 +0500

    server changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cookieSession = require('cookie-session');
-const {DateTime} = require("luxon");
+const { DateTime } = require("luxon");
 const path = require("path");
 const multer = require("multer");
 
@@ -27,17 +27,19 @@ app.use(cookieSession({
     name: 'session',
     keys: [process.env.cookieSessionKey],
     maxAge: 24 * 60 * 60 * 1000
-  }))
-app.use(bodyParser.urlencoded({extended : true}));
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-const con = mysql.createConnection({
-    user: process.env.user,
-    password: process.env.password,
-    host: process.env.host,
-    database: process.env.database,
-})
+const con = mysql.createConnection(process.env.mysqlConString)
+
+// {
+//     user: process.env.user,
+//     password: process.env.password,
+//     host: process.env.host,
+//     database: process.env.database,
+// }
 
 con.connect((err) => {
     if (err) throw err;
@@ -47,7 +49,7 @@ con.connect((err) => {
 })
 
 app.get("/", (req, res) => {
-    con.query(`INSERT INTO Users (name, email) VALUES (' ${req.query.name}', '${req.query.email}')`, (err, result) => {
+    con.query(`INSERT INTO users (name, email) VALUES (' ${req.query.name}', '${req.query.email}')`, (err, result) => {
         if (err) res.send("An error has occured.");
         else res.redirect("/general");
     });
@@ -64,18 +66,18 @@ app.get("/signup", (req, res) => {
 app.post("/signup", (req, res) => {
     bcrypt.hash(req.body.password, 10, (err, hashed_password) => {
         if (err) throw err;
-        con.query(`INSERT INTO Users (name,email,password) VALUES ('${req.body.full_name}', '${req.body.email}', '${hashed_password}')`, (err, result) => {
+        con.query(`INSERT INTO users (name,email,password) VALUES ('${req.body.full_name}', '${req.body.email}', '${hashed_password}')`, (err, result) => {
             if (err) console.log(err);
             else res.redirect("/announcements");
         });
     })
-    
+
 })
 
 app.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    con.query(`SELECT id,name,password FROM Users WHERE email='${email}'`, (err, results) => {
+    con.query(`SELECT id,name,password FROM users WHERE email='${email}'`, (err, results) => {
         if (err) res.sendStatus(500);
         else {
             const hashed_password = results[0].password;
@@ -101,13 +103,15 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage })
 
-app.post("/upload", upload.single('pfp'), (req, res) => {
-    console.log(req.file)
-    console.log("Uploaded image")
-    backURL=req.header('Referer')
-    res.redirect(backURL)
+// app.post("/upload", upload.single('pfp'), (req, res) => {
+
+app.post("/upload", (req, res) => {
+    // console.log(req.file)
+    // console.log("Uploaded image")
+    // backURL = req.header('Referer')
+    // res.redirect(backURL)
 })
 
 app.get("/logout", authMiddleware, (req, res) => {
@@ -120,23 +124,23 @@ app.get("/announcements", authMiddleware, (req, res) => {
 })
 
 app.get("/general", authMiddleware, (req, res) => {
-    res.render("feed.ejs", { user_name : req.session.user_name });
+    res.render("feed.ejs", { user_name: req.session.user_name });
 })
 
 app.get("/general-2", authMiddleware, (req, res) => {
-    res.render("feed.ejs", { user_name : req.session.user_name });
+    res.render("feed.ejs", { user_name: req.session.user_name });
 })
 
 app.get("/off-topic", authMiddleware, (req, res) => {
-    res.render("feed.ejs", { user_name : req.session.user_name });
+    res.render("feed.ejs", { user_name: req.session.user_name });
 })
 
 app.get("/support", authMiddleware, (req, res) => {
-    res.render("feed.ejs", { user_name : req.session.user_name });
+    res.render("feed.ejs", { user_name: req.session.user_name });
 })
 
 app.get("/suggestions", authMiddleware, (req, res) => {
-    res.render("feed.ejs", { user_name : req.session.user_name });
+    res.render("feed.ejs", { user_name: req.session.user_name });
 })
 
 app.post("/announcements/new", authMiddleware, (req, res) => {
@@ -200,7 +204,7 @@ app.post("/suggestions/new", authMiddleware, (req, res) => {
 })
 
 app.get("/announcements/all", authMiddleware, (req, res) => {
-    con.query("SELECT announcements_posts.id, announcements_posts.content, announcements_posts.date_time, Users.name FROM announcements_posts INNER JOIN Users ON announcements_posts.user_id=Users.id ORDER BY date_time;", (err, result) => {
+    con.query("SELECT announcements_posts.id, announcements_posts.content, announcements_posts.date_time, users.name FROM announcements_posts INNER JOIN users ON announcements_posts.user_id=users.id ORDER BY date_time;", (err, result) => {
         if (err) res.sendStatus(500)
         else {
             const final_result = result.map((message) => {
@@ -213,7 +217,7 @@ app.get("/announcements/all", authMiddleware, (req, res) => {
 })
 
 app.get("/general/all", authMiddleware, (req, res) => {
-    con.query("SELECT general_posts.id, general_posts.content, general_posts.date_time, Users.name FROM general_posts INNER JOIN Users ON general_posts.user_id=Users.id ORDER BY date_time;", (err, result) => {
+    con.query("SELECT general_posts.id, general_posts.content, general_posts.date_time, users.name FROM general_posts INNER JOIN users ON general_posts.user_id=users.id ORDER BY date_time;", (err, result) => {
         if (err) res.sendStatus(500)
         else {
             const final_result = result.map((message) => {
@@ -226,7 +230,7 @@ app.get("/general/all", authMiddleware, (req, res) => {
 })
 
 app.get("/general-2/all", authMiddleware, (req, res) => {
-    con.query("SELECT general2_posts.id, general2_posts.content, general2_posts.date_time, Users.name FROM general2_posts INNER JOIN Users ON general2_posts.user_id=Users.id ORDER BY date_time;", (err, result) => {
+    con.query("SELECT general2_posts.id, general2_posts.content, general2_posts.date_time, users.name FROM general2_posts INNER JOIN users ON general2_posts.user_id=users.id ORDER BY date_time;", (err, result) => {
         if (err) res.sendStatus(500)
         else {
             const final_result = result.map((message) => {
@@ -239,7 +243,7 @@ app.get("/general-2/all", authMiddleware, (req, res) => {
 })
 
 app.get("/off-topic/all", authMiddleware, (req, res) => {
-    con.query("SELECT offtopic_posts.id, offtopic_posts.content, offtopic_posts.date_time, Users.name FROM offtopic_posts INNER JOIN Users ON offtopic_posts.user_id=Users.id ORDER BY date_time;", (err, result) => {
+    con.query("SELECT offtopic_posts.id, offtopic_posts.content, offtopic_posts.date_time, users.name FROM offtopic_posts INNER JOIN users ON offtopic_posts.user_id=users.id ORDER BY date_time;", (err, result) => {
         if (err) res.sendStatus(500)
         else {
             const final_result = result.map((message) => {
@@ -252,7 +256,7 @@ app.get("/off-topic/all", authMiddleware, (req, res) => {
 })
 
 app.get("/suggestions/all", authMiddleware, (req, res) => {
-    con.query("SELECT suggestions_posts.id, suggestions_posts.content, suggestions_posts.date_time, Users.name FROM suggestions_posts INNER JOIN Users ON suggestions_posts.user_id=Users.id ORDER BY date_time;", (err, result) => {
+    con.query("SELECT suggestions_posts.id, suggestions_posts.content, suggestions_posts.date_time, users.name FROM suggestions_posts INNER JOIN users ON suggestions_posts.user_id=users.id ORDER BY date_time;", (err, result) => {
         if (err) res.sendStatus(500)
         else {
             const final_result = result.map((message) => {
@@ -265,7 +269,7 @@ app.get("/suggestions/all", authMiddleware, (req, res) => {
 })
 
 app.get("/support/all", authMiddleware, (req, res) => {
-    con.query("SELECT support_posts.id, support_posts.content, support_posts.date_time, Users.name FROM support_posts INNER JOIN Users ON support_posts.user_id=Users.id ORDER BY date_time;", (err, result) => {
+    con.query("SELECT support_posts.id, support_posts.content, support_posts.date_time, users.name FROM support_posts INNER JOIN users ON support_posts.user_id=users.id ORDER BY date_time;", (err, result) => {
         if (err) res.sendStatus(500)
         else {
             const final_result = result.map((message) => {
@@ -277,7 +281,7 @@ app.get("/support/all", authMiddleware, (req, res) => {
     })
 })
 
-let port=process.env.PORT||3000
+let port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`App running on port ${port} `);
